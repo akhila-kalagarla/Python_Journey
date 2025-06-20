83ec0958c5f290e8cb087556e15c59987dd5e7d3
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-06-20 13:18:21 +0530

    initial commit

--- Dsa.py
+++ Dsa.py
@@ -13,3 +13,9 @@ print("Top of the stack element is :",topelement)
 poppedelement = stack.pop()
 print("Removed element from the stack:",poppedelement)
 print(stack)
+#To know the the stack is empty or not
+s = not bool(stack)
+print(s)
+#To know the length of the stack
+n = len(stack)
+print(n)
