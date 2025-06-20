d73a651608f924a905b93bf1a47ae8ee068dd898
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-06-20 12:01:54 +0530

    initial commit

--- Dsa.py
+++ Dsa.py
@@ -4,4 +4,12 @@ stack.append('Venkata Ramana')
 stack.append('Rohini')
 stack.append('Akhila')
 stack.append('Kishor')
+stack.append('Naya sri')
 print("Family of Kalagarla's:",stack)
+#To print peek element from the stack
+topelement = stack[-1]
+print("Top of the stack element is :",topelement)
+#To remove the element from the stack
+poppedelement = stack.pop()
+print("Removed element from the stack:",poppedelement)
+print(stack)
