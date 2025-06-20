42d7be49e2436ac9d57a0b2c24dd395f6a961698
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-06-20 13:44:39 +0530

    initial commit

--- Dsa.py
+++ Dsa.py
@@ -19,3 +19,36 @@ print(s)
 #To know the length of the stack
 n = len(stack)
 print(n)
+
+# creating a dedicated Stack class provides better encapsulation and additional functionality:
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, element):
+        self.stack.append(element)
+    def pop(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack.pop()
+    def peek(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack[-1]
+    def isEmpty(self):
+        return len(self.stack) == 0
+    def size(self):
+        return len(self.stack)
+
+mystack = Stack()
+mystack.push('Chitti babu')
+mystack.push('kantham')
+mystack.push('Ajay')
+mystack.push('Pushpa')
+mystack.push('Surimma')
+print("Family of Kolli's:",mystack.stack)
+print("peek element",mystack.peek())
+print("popped element:",mystack.pop())
+print("length of the stack:",mystack.size())
+print("Stack is empty or not:", mystack.isEmpty())
+print("Stack after operations:", mystack.stack)
+
