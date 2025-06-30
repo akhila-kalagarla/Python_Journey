e50caa80c598c8c11c566a3e25444b21b3fdc3e2
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-06-30 10:56:56 +0530

    initial commit

--- dailyclasses.py
+++ dailyclasses.py
@@ -1,3 +1,4 @@
+#Numeric data types
 a = 10
 print(a) #10
 a = 10
@@ -8,3 +9,14 @@ print(type(a)) #<class 'float'>
 a = 5+3j
 print(a) #(5+3j)
 print(type(a)) #<class 'complex'>
+
+#Text data type
+name = 'Akhila' 
+quote = "old is gold"
+multi = '''This is a multi line string'''
+print(name) #Akhila
+print(quote) #old is gold
+print(multi) #This is a multi line string
+print(type(name)) #<class 'str'>
+print(type(quote)) #<class 'str'>
+print(type(multi)) #<class 'str'>
