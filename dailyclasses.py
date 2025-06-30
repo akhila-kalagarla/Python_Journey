a36b7997fbc24aa0443a51461586cdc75519ff91
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-06-30 10:48:55 +0530

    initial commit

---
+++ dailyclasses.py
@@ -0,0 +1,10 @@
+a = 10
+print(a) #10
+a = 10
+print(type(a)) #<class 'int'>
+a = 10.5
+print(a) #10.5
+print(type(a)) #<class 'float'>
+a = 5+3j
+print(a) #(5+3j)
+print(type(a)) #<class 'complex'>
