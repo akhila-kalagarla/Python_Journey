15e4c1f083f52766c560ce4adebd86862cc37ab9
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-06-30 11:16:52 +0530

    initial commit

--- dailyclasses.py
+++ dailyclasses.py
@@ -1,4 +1,4 @@
-#Numeric data types
+###Numeric data types
 a = 10
 print(a) #10
 a = 10
@@ -10,7 +10,7 @@ a = 5+3j
 print(a) #(5+3j)
 print(type(a)) #<class 'complex'>
 
-#Text data type
+###Text data type
 name = 'Akhila' 
 quote = "old is gold"
 multi = '''This is a multi line string'''
@@ -20,3 +20,27 @@ print(multi) #This is a multi line string
 print(type(name)) #<class 'str'>
 print(type(quote)) #<class 'str'>
 print(type(multi)) #<class 'str'>
+
+###Set data types - Unorderd,{},Unique
+set = {1,2,5,2,4}
+print(set) #{1, 2, 4, 5}
+
+###Boolean data type
+a = 2
+print(bool(a)) #True
+a = []
+print(bool(a)) #False
+
+###Sequence data types
+#LIST - [],Mutable,Ordered
+list = [10,20,"kiet",30]
+print(list) #[10, 20, 'kiet', 30]
+print(list[0]) #10
+list[0] = 80
+print(list) #[80, 20, 'kiet', 30]
+#TUPLES - (),Ordered, Immutable
+tup = (10,20,"kiet",50)
+print(tup) #(10, 20, 'kiet', 50)
+
+###Mapping data type
+#Dictionary - {},
