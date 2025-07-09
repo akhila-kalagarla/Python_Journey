223b3beabc9f9e8d26201bc9ca366c7e31994f99
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-09 15:21:21 +0530

    initial commit

--- functions.py
+++ functions.py
@@ -16,3 +16,25 @@ indentend block-   code that runs when the fucntion is called
 def hi():
     print("Hello students")
 hi()    # Hello students
+#Ex1:-
+def students():
+    return 80
+total = students()
+print("Total students :",total)  # Total students : 80
+#Ex2-without using function calling:-
+def stu():
+    return 80
+tot = stu()
+print("Total students :",80) # Total students : 80
+'''Function Arguments
+3types of arguments
+1.positional 2.Keyword 3.Default
+#ex-on positional'''
+def hi(name):
+    print("hello",name)
+hi("vijju")  # hello vijju
+'''#ex- on Default '''
+def hi(name="friends"):
+    print("Hello",name)
+hi()        # Hello friends
+hi("Akki")  # Hello Akki
