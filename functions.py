e5b6320e88433012045d4b766095399a8d96fbf1
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-09 14:58:10 +0530

    initial commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+#Functions
+#Syntax
+'''def function_name(parameters):
+        #block of code
+        return value  #(optional)'''
+
+'''Term        -        Description
+    def        -   keyword to define function
+function_name  -   Any valid python identifier
+  parameters   -   input passes to the function
+     :         -   Ends the function header
+indentend block-   code that runs when the fucntion is called
+   return      -   Sends result back to caller '''
+
+#Example:-
+def hi():
+    print("Hello students")
+hi()    # Hello students
