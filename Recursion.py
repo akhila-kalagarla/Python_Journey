d2b422f372c24b0d687448fb4a88dfb99f695ea0
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-11 14:33:20 +0530

    initial commit

---
+++ Recursion.py
@@ -0,0 +1,16 @@
+# Recursion
+def x(n):
+    if n == 0:
+        print("Hello off")
+        return
+    print(n, end = ",")
+    x(n-1)
+x(5)
+
+#Ex2:-
+def fact(n):
+    if n == 0 or n==1:
+        return 1
+    return n*fact(n-1)
+
+print(fact(4)) # 24
--- scope.py
+++ scope.py
@@ -55,19 +55,3 @@ def students(**kwargs):
     print(kwargs)
 students(name = "Akki", age = 19) # {'name': 'Akki', 'age': 19}
 
-# Recursion
-def x(n):
-    if n == 0:
-        print("Hello off")
-        return
-    print(n, end = ",")
-    x(n-1)
-x(5)
-
-#Ex2:-
-def fact(n):
-    if n == 0 or n==1:
-        return 1
-    return n*fact(n-1)
-
-print(fact(4)) # 24
