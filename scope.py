df24d81a2c504fd5542c095743964d634c48755c
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-11 13:32:00 +0530

    initial commit

--- scope.py
+++ scope.py
@@ -1,15 +1,26 @@
-#Local scope:-
-def hi():
-    name = "Akki"
-    print("Hello",name) # Hello Akki
-hi()
+# #Local scope:-
+# def hi():
+#     name = "Akki"
+#     print("Hello",name) # Hello Akki
+# hi()
 
-#Global Scope
-x = 10
-def show():
-    x = 2
-    print(x)
+# #Global Scope
+# x = 10
+# def show():
+#     x = 2
+#     print(x)
 
-show()
-x = 5
-print(x)
+# show()
+# x = 5
+# print(x)
+
+#Enclosed scope(Non Local)
+def outer():
+    x = 10
+    def inner():
+        nonlocal x
+        x += 5
+        print("Inner",x)
+    inner()
+    print("Outer",x)
+outer()   
