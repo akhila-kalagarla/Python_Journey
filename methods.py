42bcc4603e443024f36a0769abc67928bae4bb66
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-14 13:43:28 +0530

    initial commit

---
+++ methods.py
@@ -0,0 +1,3 @@
+#String 
+'''A string is a squence of characters enclosed in single (''), double (""), or triple quotes (''' ''').
+Strings are immutable, meaning their characters cannot be changed once created'''
