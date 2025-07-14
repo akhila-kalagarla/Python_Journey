0563e0d4c1bfccce9a089e99b1a2b0d71efe0743
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-14 13:54:36 +0530

    initial commit

--- methods.py
+++ methods.py
@@ -1,3 +1,13 @@
 #String 
 '''A string is a squence of characters enclosed in single (''), double (""), or triple quotes (''' ''').
 Strings are immutable, meaning their characters cannot be changed once created'''
+str = '''hello'''
+print(type(str))
+
+#String operations
+#Access:
+s = "Hello world"
+print(s[0]) # H
+print(s[0:5]) # Hello
+print(s+"!") # HEllo world!
+print(s*2) # Hello worldHello world
