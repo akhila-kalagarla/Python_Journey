3d5f776da15f3747e7201ded23ccc8b979b0f3f7
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-17 14:52:15 +0530

    initial commit

--- list.py
+++ list.py
@@ -12,55 +12,47 @@ print(list[::-1])
 print(list[::-2])
 
 '''List methods'''
+
 '''Append method'''
 lst = [10,20,30]
 lst.append(20) # [10, 20, 30, 20]
 lst.append([40,50]) # [10, 20, 30, 20, [40, 50]]
 print(lst)
-
 '''Extend method'''
 lst = [10,20,30]
 lst.extend([40,50,60]) # [10, 20, 30, 40, 50, 60]
 k = [70,80,90]
 lst.extend(k) # [10, 20, 30, 40, 50, 60, 70, 80, 90]
 print(lst)
-
 '''Insert method'''
 lst = [1,2,3,4,5]
 lst.insert(0,0)
 print(lst) # [0, 1, 2, 3, 4, 5]
-
 '''Remove method'''
 lst = [1,2,3,4,5]
 lst.remove(5)
 print(lst) # [1, 2, 3, 4]
-
 '''copy method'''
 lst = [1,2,3,4,5]
 a = lst.copy()
 print(a) # [1, 2, 3, 4, 5]
-
 '''pop method'''
 lst = [1,2,3,4,5]
 a = lst.pop() # # [1, 2, 3, 4]
 lst.pop(3) # [1, 2, 3]
 print(lst) 
-
 '''count method'''
 lst = [1,2,3,4,5,3,3,3]
 a = lst.count(3) 
 print(a) # 4
-
 '''index method'''
 lst = [1,2,3,4,5,3,3,3]
 a = lst.index(4) 
 print(a)  # 3
-
 '''clear'''
 lst = [1,2,3,4,5,3,3,3]
 lst.clear()
 print(lst) # []
-
 '''sort method'''
 lst = [1,4,2,3,5]
 lst.sort()
@@ -69,7 +61,6 @@ print(*lst) # 1 2 3 4 5
 lst = [1,4,2,3,5]
 lst.sort(reverse=True)
 print(lst) # [5, 4, 3, 2, 1]
-
 '''reverse method'''
 lst = [10,20,30,40]
 lst.reverse()
