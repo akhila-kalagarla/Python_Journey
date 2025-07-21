0331343b86bb460262e23c2e4bb48d8c2e6559d8
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-21 14:40:00 +0530

    initial commit

--- Examples.py
+++ Examples.py
@@ -2,13 +2,13 @@
 a = [1,2,3]
 b = a
 b.append(4)
-print(a)
-print(b)
+print(a) # [1, 2, 3, 4]
+print(b) # [1, 2, 3, 4]
 #OR
 x = [1,2,3]
 y = x[:]
 y[0] = 100
-print(x)
+print(x) # [1, 2, 3]
 
 # It returns
 print(tuple("hello")) # ('h', 'e', 'l', 'l', 'o')
