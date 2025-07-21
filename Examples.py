962e3d055beaa40029a36d3a8864fb6fdc2ebe4c
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-21 14:03:27 +0530

    initial commit

--- Examples.py
+++ Examples.py
@@ -1,10 +1,14 @@
+# To append the elemnets in already intializes list
 a = [1,2,3]
 b = a
 b.append(4)
 print(a)
 print(b)
-
+#OR
 x = [1,2,3]
 y = x[:]
 y[0] = 100
 print(x)
+
+# It returns
+print(tuple("hello")) # ('h', 'e', 'l', 'l', 'o')
