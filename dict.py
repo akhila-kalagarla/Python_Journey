e562d23ee327f466f75b10eb1b8a0055ca6a90e5
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-25 13:38:22 +0530

    first commit

---
+++ dict.py
@@ -0,0 +1,7 @@
+'''Dictionaries - are denoted by key value pairs and every pair is separated by comma'''
+phn = {"jhon":97048227682,"smith":98270456902,"akhila":9676455883}
+print(phn["smith"]) # 98270456902
+
+a = {1:"a",2:"b",3:"c",4:"d"}
+print(a[1]) # v
+
