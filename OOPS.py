13d5333077e6d560e554c3655cba29d7ba3bef8a
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-25 22:08:49 +0530

    first commit

--- OOPS.py
+++ OOPS.py
@@ -1,3 +1,75 @@
+#A class and pbject  in oops
+'''Class : A blueprint for objects
+   object : An instanceof the class'''
+class person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def greet(self):
+        print(f"hello my name is {self.name} and i am {self.age} years old")
+p1 = person("Akhila",20)
+p2 = person("Pushpa",21)
+print(p1.name) # Akhila
+print(p2.name) # Pushpa
+p1.greet() # hello my name is Akhila and i am 20 years old
+p2.greet() # hello my name is Pushpa and i am 21 years old
+
+
+# Encapsulation
+'''Hiding internal data by making variables private'''
+class student:
+    def __init__(self, name, marks):
+        self.__name = name
+        self.__marks = marks
+    def details(self):
+        return f"{self.__name} got {self.__marks} marks"
+s1 = student("Akhila",95)
+s2 = student("Nandu",98)
+print(s1.details())
+# print(s1.name)
+
+#Inheritance
+'''One class inherits properties/methods of another'''
+class Animal:
+    def speak(self):
+        print("Animal sounds")
+class dog(Animal):
+    def speak(self):
+        print("Dog barks")
+class cat(Animal):
+    def speak(self):
+        print("cat Meow")
+d = cat()
+a = dog()
+a.speak()
+d.speak()
+
+#Polymorphism
+'''Same method name, different behavior based on object'''
+class Cat:
+    def sound(self):
+        print("Cat meows")
+class Cow:
+    def sound(self):
+        print("cow moos")
+def sounds(animal):
+    animal.sound()
+sounds(Cat())
+sounds(Cow())
+
+# Abstraction
+'''Hiding complex logic, showing only important information'''
+from abc import ABC, abstractmethod
+class vehical(ABC):
+    @abstractmethod
+    def start(self):
+        pass
+class Bike(vehical):
+    def start(self):
+        print("Bike started")
+b = Bike()
+b.start() # Bike started
+
 '''Real time example to understand the oops concept'''
 class Animal:
     def __init__(self, name):
@@ -39,4 +111,3 @@ student2 = Student("Nandu", 102)
 student1.display_info()
 print("------------------")  # Just to add a blank line
 student2.display_info()
-
--- dict.py
+++ dict.py
@@ -4,6 +4,9 @@ print(phn["smith"]) # 98270456902
 
 # Examples
 a = {1:"a",2:"b",3:"c",4:"d"}
+print(a.keys()) # dict_keys([1, 2, 3, 4])
+print(a.values()) # dict_values(['a', 'b', 'c', 'd'])
+print(a.items()) # dict_items([(1, 'a'), (2, 'b'), (3, 'c'), (4, 'd')])
 print(a[1]) # v
 print(a.get(4)) # d
 a.update({2:"e"})
@@ -26,4 +29,36 @@ print(dict) # {'1': 'akki', '2': 'nandu', '3': 'naya', '4': 'bharath'}
 d1 = dict.copy()
 print(d1)
 
+'''To print the square numbers of a values'''
+d = {1:2, 2:3, 3:4, 4:5}
+for value in d.values():
+    print(value**2,end = " ") # 4 9 16 25
+'''Using user input'''
+print()
+n = int(input())
+d = {}
+for _ in range(n):
+    k = int(input())
+    v = int(input())
+    d[k] = v*v
+print(d.values()) # dict_values([1, 4, 9, 16])
 
+'''Write a program to take a list of word and print the dictionary with the words as keys and the length of the words as values'''
+n = list(map(str, input().split()))
+d = {}
+for word in n:
+    if len(word) % 2 == 0:
+        d[word] = len(word)
+print(d) # {'akhila': 6, 'nandhu': 6, 'nayu': 4}
+# Or
+n = int(input())
+arr = []
+for i in range(n):
+    a = input()
+    arr.append(a)
+print(arr) # ['akhila', 'nandhu', 'nayu']
+d = {}
+for i in arr:
+    if len(i) % 2 == 0:
+        d[i] = len(i)
+print(d) # ['akhila', 'nandhu', 'nayu']
