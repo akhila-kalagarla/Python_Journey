762c3641ba47ad8c14be054f2ba81d4957335dae
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-07-30 15:01:10 +0530

    first commit

--- excep.py
+++ excep.py
@@ -1,2 +1,18 @@
-# Exception handling
-'''Handling the errors without getting any errors in output'''
+# # Exception handling
+# '''Handling the errors without getting any errors in output'''
+# '''1.example on zerodivision error'''
+# a = int(input())
+# b = 0
+# try:
+#     c = a/b
+# except ZeroDivisionError:
+#     print("zero")
+# '''2.To check your age is valid or not'''
+# age = int(input("Enter your age:")) # 61
+# try:
+#     if age < 18:
+#         raise ValueError;
+#     else:
+#         print("The age",age, "is valid") # The age 61 is valid
+# except:
+#     print("The age",age, "is not valid")
---
+++ leetcode.py
@@ -0,0 +1,13 @@
+'''1.To print the product of elements in the given array except the self element'''
+nums = [1,2,3,4]
+n = len(nums)
+op = [1]*n
+prefix = 1
+for i in range(n):
+    op[i] = prefix
+    prefix *= nums[i]
+suffix = 1
+for i in range(n -1, -1, -1):
+    op[i] *= suffix
+    suffix *= nums[i]
+print(op)
