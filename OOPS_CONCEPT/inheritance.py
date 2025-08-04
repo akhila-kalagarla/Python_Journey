a95883fc319a6ad66e29cafa73a41a9e5d169da1
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-08-04 15:17:56 +0530

    Initial commit

--- OOPS_CONCEPT/inheritance.py
+++ OOPS_CONCEPT/inheritance.py
@@ -1,7 +1,7 @@
 '''OVERRIDING - sub class rewrites the parent class code'''
 #Inheritance
 '''-One class inherits properties/methods of another
-   Inheritance is classified into 2 types
+   Inheritance is classified into 5t types
    - 1)Single
    - 2)Multilevel
    - 3)Multiple
@@ -72,11 +72,35 @@ obj.greet() # Hii from child
 obj.greek() # Hi from parent
 obj.greeks() # Hi from grandparent
 
-#Example on Hierarchial inheritance
+#Example on Hierarchial inheritance - Using parent class we write multiple sub classes
+class parent:
+    def greet(self):
+        print("Hello from parent")
+class child(parent):
+    def feature1(self):
+        print("feature1 from child1")
+class child2(parent):
+    def feature2(self):
+        print("feature2 from child2")
+obj1 = child()
+obj2 = child2()
+obj1.greet() # Hello from parent
+obj1.feature1() # feature1 from child1
+obj2.greet() # Hello from parent
+obj2.feature2() # feature2 from child2
 
-#Example on Hybrid inheritance
+# Using super keyword to print the output from the parent class and sub class
+class humans:
+    def greet(self):
+        print("Hi from humans")
+class car(humans):
+    def greet(self):
+        super().greet()
+        print("i am in house")
+obj = car()
+obj.greet()
 
-'''Example for inheritance concept'''
+'''Example on Inheritance'''
 class Animal:
     def speak(self):
         print("Animal sounds")
