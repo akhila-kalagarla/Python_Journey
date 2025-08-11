794180f409a450e3239c4c55279360974a839cb8
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-08-11 13:55:28 +0530

    Initial commit

--- OOPS_CONCEPT/Abstraction.py
+++ OOPS_CONCEPT/Abstraction.py
@@ -1,1 +1,6 @@
-'''ABSTRACTION - Displaying only essential features and hiding unnecessary details'''
+'''ABSTRACTION - Displaying only essential features and hiding unnecessary details
+Definition: Abstraction is a fundamental concept in object-oriented programming that focuses on exposing only the necessary parts of an object while hiding the complex implementation details. 
+This allows users to interact with objects at a higher level without needing to understand the underlying complexities.'''
+#Difference between Abstraction and Encapsulation:
+'''Abstraction is about hiding the complexity by providing a simplified interface, 
+while encapsulation is about bundling the data and methods that operate on that data within a single unit (class).'''
