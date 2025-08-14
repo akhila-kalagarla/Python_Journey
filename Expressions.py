5f695aa6dde50930e39cb5ee7a656cc8b43dbedc
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-08-14 14:07:17 +0530

    Initial commit

--- Expressions.py
+++ Expressions.py
@@ -5,4 +5,14 @@ some regular expression operations include searching, replacing, and splitting s
 # Importing the re module
 # re module provides support for regular expressions in Python.
 # It allows you to search, match, and manipulate strings using patterns.'''
-# [] - It represents a set of characters.
+                # [] - It represents a set of characters.
+                # \t - It is used for tab character.
+                # \n - It is used for newline character.
+                # \d - It matches any digit character (equivalent to [0-9]).
+                # .  - It defines a specific place 
+                # ^  - It matches the start of a string.
+                # $  - It matches the end of a string.
+                # *  - It matches zero or more occurrences of the preceding element.
+                # +  - It matches one or more occurrences of the preceding element.
+                # ?  - It matches zero or one occurrence of the preceding element.
+                
