1521135622bcc81866ab72bb22c39a4f94ba5a02
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-08-14 13:57:39 +0530

    Initial commit

---
+++ Expressions.py
@@ -0,0 +1,8 @@
+'''Regular expressions - a module for working with regular expressions in Python.
+Defination - A regular expression (regex or regexp) is a sequence of characters that defines a search pattern, primarily used for string matching and manipulation.
+some regular expression operations include searching, replacing, and splitting strings based on patterns.
+# Example of using regular expressions in Python
+# Importing the re module
+# re module provides support for regular expressions in Python.
+# It allows you to search, match, and manipulate strings using patterns.'''
+# [] - It represents a set of characters.
