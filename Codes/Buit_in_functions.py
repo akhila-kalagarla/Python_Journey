a03748f9522a8b7597fb2fa215772e7ae6c3329d
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-08-18 14:06:01 +0530

    Initial commit

--- Codes/Buit_in_functions.py
+++ Codes/Buit_in_functions.py
@@ -1,2 +1,16 @@
 #Buit-in-functions
-'''Built in functions are the functions that are already defined in python. '''
+'''Built in functions are the functions that are already defined in python.'''
+#Example of built-in functions
+import math 
+print(math.sqrt(16)) # 4.0
+print(math.pow(2,3)) # 8.0
+print(math.gcd(8,16,24)) # 8
+print(math.factorial(5)) # 120
+print(math.pi) # 3.141592653589793 
+print(math.e) # 2.718281828459045
+print(math.ceil(4.2)) # 5   
+print(math.floor(4.8)) # 4
+print(math.trunc(4.8)) # 4
+print(math.fabs(-4.8)) # 4.8
+print(math.sin(math.pi/2)) # 1.0
+    
