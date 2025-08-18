81b13eaa85dd5ba27a730b304e6369f1e2fb40ba
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-08-18 14:25:18 +0530

    Initial commit

--- Codes/Buit_in_functions.py
+++ Codes/Buit_in_functions.py
@@ -1,4 +1,4 @@
-#Buit-in-functions
+#Bui t-in-functions
 '''Built in functions are the functions that are already defined in python.'''
 #Example of built-in functions
 import math 
@@ -15,3 +15,5 @@ print(math.fabs(-4.8)) # 4.8
 print(math.degrees(math.pi)) # 180.0
 print(math.radians(180)) # 3.141592653589793
 print(math.ceil(18.6543)) # 19
+print(math.gcd(10,30)) # 10 
+print(math.lcm(2,5,10)) # 10 
