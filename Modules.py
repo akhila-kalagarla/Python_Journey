00e36855f2e9b40be706ccc9b8384701d6c9f0ac
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-08-19 13:45:16 +0530

    Initial commit

--- Modules.py
+++ Modules.py
@@ -1,2 +1,10 @@
 #Modules in python 
-''''''
+'''Modules are files containing Python code. They can define functions, classes, and variables that can be reused in other Python programs.
+You can import a module using the import statement.'''
+import math as m
+print(m.sqrt(16))  # 4.0
+print(m.pi)         # 3.141592653589793
+print(m.factorial(5))  # 120
+from math import floor, ceil
+print(ceil(3.7))  # 4
+print(floor(3.7))  # 3
