23d422634d21f177d5e6156dc2c2d4a7fb9006cc
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-09-08 18:21:47 +0530

    Initial commit

--- DSA/Linkedlist.py
+++ DSA/Linkedlist.py
@@ -1,13 +1,13 @@
-# class Node:
-#     def __init__(self, value = 0, next_node = None ):
-#         self.data = value
-#         self.next = next_node 
-# p1 = Node(10,None )
-# p2 = Node(20)
-# p3 = Node()
-# print(p1.data)
-# print(p2.data)6
-# print(p3.data)
+class Node:
+    def __init__(self, value = 0, next_node = None ):
+        self.data = value
+        self.next = next_node 
+p1 = Node(10,None )
+p2 = Node(20)
+p3 = Node()
+print(p1.data)
+print(p2.data)
+print(p3.data)
 
 class Node:
     def __init__(self, data):   
