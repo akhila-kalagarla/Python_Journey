4c57c775d48887ac36fa3a023ecf7c18746b8627
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-09-09 16:00:43 +0530

    initial commit

--- DSA/Stack.py
+++ DSA/Stack.py
@@ -1,71 +1,71 @@
-'''Stack - A stack is a linear data structure that follows the Last In First Out (LIFO) principle.
-It means the last element added to the stack is the first one to be removed. 
-Stacks are used in various applications such as function call management, expression evaluation, and backtracking algorithms.
-Stack operations include:
+# '''Stack - A stack is a linear data structure that follows the Last In First Out (LIFO) principle.
+# It means the last element added to the stack is the first one to be removed. 
+# Stacks are used in various applications such as function call management, expression evaluation, and backtracking algorithms.
+# Stack operations include:
 
-1. Push: Add an element to the top of the stack.
-2. Pop: Remove the top element from the stack.
-3. Peek: Retrieve the top element without removing it.
-4. isEmpty: Check if the stack is empty.
-5. Size: Get the number of elements in the stack.
-Stack can be implemented using lists or linked lists in Python.
-In Python, you can use a list to implement a stack, as it provides built-in methods like append() for push and pop() for pop operations.
+# 1. Push: Add an element to the top of the stack.
+# 2. Pop: Remove the top element from the stack.
+# 3. Peek: Retrieve the top element without removing it.
+# 4. isEmpty: Check if the stack is empty.
+# 5. Size: Get the number of elements in the stack.
+# Stack can be implemented using lists or linked lists in Python.
+# In Python, you can use a list to implement a stack, as it provides built-in methods like append() for push and pop() for pop operations.
 
-Overflow and underflow conditions can be handled by checking the size of the stack before performing push or pop operations.
-Overflow occurs when trying to push an element onto a full stack. 
-Underflow occurs when trying to pop an element from an empty stack.
+# Overflow and underflow conditions can be handled by checking the size of the stack before performing push or pop operations.
+# Overflow occurs when trying to push an element onto a full stack. 
+# Underflow occurs when trying to pop an element from an empty stack.
 
-# Implementation of stack 
-using Array (list) in Python
-using linked list in Python - Deque from collections, LIFO Queue
-'''
-
-
-# To create a stack using list
-stack = []
-#To add elements to the stack
-stack.append('Venkata Ramana')
-stack.append('Rohini')
-stack.append('Akhila')
-stack.append('Kishor')
-stack.append('Naya sri')
-print("Family of Kalagarla's:",stack)
-#To print peek element from the stack
-topelement = stack[-1]
-print("Top of the stack element is :",topelement)
-#To remove the element from the stack
-poppedelement = stack.pop()
-print("Removed element from the stack:",poppedelement)
-print(stack)
-#To know the the stack is empty or not
-s = not bool(stack)
-print(s)
-#To know the length of the stack
-n = len(stack)
-print(n)
-# To know the the stack is empty or not
-is_empty = len(stack) == 0
-print(is_empty)
+# # Implementation of stack 
+# using Array (list) in Python
+# using linked list in Python - Deque from collections, LIFO Queue
+# '''
 
 
+# # To create a stack using list
+# stack = []
+# #To add elements to the stack
+# stack.append('Venkata Ramana')
+# stack.append('Rohini')
+# stack.append('Akhila')
+# stack.append('Kishor')
+# stack.append('Naya sri')
+# print("Family of Kalagarla's:",stack)
+# #To print peek element from the stack
+# topelement = stack[-1]
+# print("Top of the stack element is :",topelement)
+# #To remove the element from the stack
+# poppedelement = stack.pop()
+# print("Removed element from the stack:",poppedelement)
+# print(stack)
+# #To know the the stack is empty or not
+# s = not bool(stack)
+# print(s)
+# #To know the length of the stack
+# n = len(stack)
+# print(n)
+# # To know the the stack is empty or not
+# is_empty = len(stack) == 0
+# print(is_empty)
 
-#To create a stack using deque
-from collections import deque
-stack = deque()
-#To add elements to the stack
-stack.append('Venkata Ramana')
-stack.append('Rohini')
-stack.append('Akhila')
-stack.append('Kishor')
-stack.append('Naya sri')
-print("Family of Kalagarla's:",stack)
-#To print peek element from the stack
-topelement = stack[-1]
-print("Top of the stack element is :",topelement)
-#To remove the element from the stack
-poppedelement = stack.pop()
-print("Removed element from the stack:",poppedelement)
-print(stack)
+
+
+# #To create a stack using deque
+# from collections import deque
+# stack = deque()
+# #To add elements to the stack
+# stack.append('Venkata Ramana')
+# stack.append('Rohini')
+# stack.append('Akhila')
+# stack.append('Kishor')
+# stack.append('Naya sri')
+# print("Family of Kalagarla's:",stack)
+# #To print peek element from the stack
+# topelement = stack[-1]
+# print("Top of the stack element is :",topelement)
+# #To remove the element from the stack
+# poppedelement = stack.pop()
+# print("Removed element from the stack:",poppedelement)
+# print(stack)
 
 
 
