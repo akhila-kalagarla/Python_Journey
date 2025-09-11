c0257bf5e0654abad9bde6f51307278e4d6f4f22
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-09-11 15:44:55 +0530

    initial commit

---
+++ DSA/LinkedList/Linkedlist1.py
@@ -0,0 +1,72 @@
+# class Node:
+#     def __init__(self, data):
+#         self.data = data 
+#         self.next = None
+# head = Node(1)
+# tail = head 
+# tail.next = Node(2)
+# tail = tail.next
+# tail.next = Node(3)
+# tail = tail.next
+# new_node = Node(4)
+# new_node.next = head
+# head = new_node
+# print(head.data,tail.data,head.next.data,head.next.next.data)
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class linkedlist:
+    def __init__(self):
+        self.tail = None
+        self.head = None
+    def addlast(self,data):
+        new_node = Node(data)
+        if self.head == None and self.tail == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = self.tail.next
+    def add(self,data):
+        new_node = Node(data)
+        if self.head == None and self.tail == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+    def printdata(self):
+        temp = self.head
+        while temp!= None:
+            print(temp.data,end="->")
+            temp = temp.next
+    def findatindex(self,index):
+        temp = self.head   
+        i = 0
+        while i < index:
+            temp = temp.next
+            i+=1
+        return temp 
+    def insert(self,index,data):
+        if index == 0:
+            self.add(data)
+            return
+        new_node = Node(data)
+        prev = self.findatindex(index-1)
+        new_node.next = prev.next
+        prev.next = new_node
+        if new_node.next == None:
+            self.tail = new_node
+
+l1 = linkedlist()   
+l1.addlast(10)
+l1.addlast(20)
+l1.addlast(30)
+l1.add(40)
+l1.insert(2,25)
+l1.printdata()
+res = l1.findatindex(4)
+print()
+print(res.data)
