fece6d5f17f361e3b43425a83c3c33c7fac35e5c
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-09-15 14:02:39 +0530

    initial commit

---
+++ DSA/LinkedList/Double_linkedlist.py
@@ -0,0 +1,4 @@
+''' Double linked list - A linked list in which each node contains a reference (link) to the next node as well as to the previous node. 
+This allows for traversal in both directions (forward and backward) through the list.
+This is in contrast to a singly linked list, where each node only contains a reference to the next node, allowing for traversal in only one direction.
+ '''
