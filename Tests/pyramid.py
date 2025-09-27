aaf54aa7069552db5606eeac4a23996afcb1fff5
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-09-27 10:33:21 +0530

    initial commit

--- Tests/pyramid.py
+++ Tests/pyramid.py
@@ -1,7 +1,6 @@
 # To find the width of the two sections of a pyramid given the total width and the width of the vertical section
 v = int(input().strip())
 w = int(input().strip())
-
 if w % 2 != 0 or v < 2 or v > 500 or w < 2 or w > 1000 or v > w:
     print("INVALID INPUT")
 else:
@@ -23,7 +22,6 @@ print(count_star - count_hash)
 # To find the number of Sundays in the given number of days starting from a specific day
 start_day = input().strip().lower()
 n = int(input().strip())
-
 days_map = {
     "mon": 0,
     "tue": 1,
@@ -33,11 +31,8 @@ days_map = {
     "sat": 5,
     "sun": 6
 }
-
 start_index = days_map[start_day]
 sundays = n // 7
-
 if (start_index + (n % 7) - 1) >= 6:  
     sundays += 1
-
 print(sundays)
