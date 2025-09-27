f97214fbf5c3ce3c66cdbc21e966dfdd4a19dfb0
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-09-27 10:33:00 +0530

    initial commit

--- Tests/pyramid.py
+++ Tests/pyramid.py
@@ -1,3 +1,4 @@
+# To find the width of the two sections of a pyramid given the total width and the width of the vertical section
 v = int(input().strip())
 w = int(input().strip())
 
@@ -11,3 +12,32 @@ else:
     else:
         print(f"TW= {tw} FW= {fw}")
 
+
+# To find the difference between the count of '*' and '#' in the given string
+s = input().strip()
+count_star = s.count('*')
+count_hash = s.count('#')
+print(count_star - count_hash)
+
+
+# To find the number of Sundays in the given number of days starting from a specific day
+start_day = input().strip().lower()
+n = int(input().strip())
+
+days_map = {
+    "mon": 0,
+    "tue": 1,
+    "wed": 2,
+    "thu": 3,
+    "fri": 4,
+    "sat": 5,
+    "sun": 6
+}
+
+start_index = days_map[start_day]
+sundays = n // 7
+
+if (start_index + (n % 7) - 1) >= 6:  
+    sundays += 1
+
+print(sundays)
