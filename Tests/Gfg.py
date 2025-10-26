6f5e046f284aeccc255826b188eea190bcfa7c43
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-10-26 10:28:59 +0530

    first commit

--- Tests/Gfg.py
+++ Tests/Gfg.py
@@ -1,41 +1,40 @@
-# #To remove special symbols from the given text
-# def process(text):
-#     text = ''.join(e for e in text if e.isalnum() or e.isspace())
-#     text = text.strip()
-#     return text
-# result = input("Enter a text:")   #Enter a text:###Hello i am Akhila from @Kiet-W with the roll no: 22JN1A4506,!
-# print(process(result)) #Output: Hello i am Akhila from KietW with the roll no 22JN1A4506
+#To remove special symbols from the given text
+def process(text):
+    text = ''.join(e for e in text if e.isalnum() or e.isspace())
+    text = text.strip()
+    return text
+result = input("Enter a text:")   #Enter a text:###Hello i am Akhila from @Kiet-W with the roll no: 22JN1A4506,!
+print(process(result)) #Output: Hello i am Akhila from KietW with the roll no 22JN1A4506
 
-# #To print a,c values
-# def num(a,b,c):
-#     print(a)
-#     #print(b)
-#     print(c)
-#     return [a,c]
-# a = int(input())
-# b = int(input())
-# c = int(input())
-# print(num(a,b,c))
+#To print a,c values
+def num(a,b,c):
+    print(a)
+    #print(b)
+    print(c)
+    return [a,c]
+a = int(input())
+b = int(input())
+c = int(input())
+print(num(a,b,c)) #[a,c] values will be printed 
 
-# #Swapping of 2 variables
-# x = 5
-# y = 6
-# temp = x
-# x = y
-# y = temp
-# print(x)
-# print(y)
-#   #Or
-# x, y = y, x
-# print(x)
-# print(y)
-#   #Or
-# x = x + y
-# y = x - y
-# x = x - y
-# print(x)
-# print(y)
-
+#Swapping of 2 variables
+x = 5
+y = 6
+temp = x
+x = y
+y = temp
+print(x)
+print(y)
+  #Or
+x, y = y, x
+print(x)
+print(y)
+  #Or
+x = x + y
+y = x - y
+x = x - y
+print(x)
+print(y)
 
 # #Expressions on the right-hand side are evaluated before assignment:
 x = 100
@@ -45,3 +44,11 @@ x, y = x - 1, y + "6"
 print(x)
 print(y)
 
+# To check Truthy values
+x = 7
+if x:
+  print(x,"is a number")
+
+x = 0
+if x:
+   print(x,"is a number")
