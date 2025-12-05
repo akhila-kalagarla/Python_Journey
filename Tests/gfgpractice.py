0bf4703e6e3284138270fbaaece62b089bceab44
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-12-05 10:29:43 +0530

    first commit

--- Tests/gfgpractice.py
+++ Tests/gfgpractice.py
@@ -1,26 +1,26 @@
-# x = 5
-# print(x)
-# y = x 
-# x = "Geeks"
-# print(x,y)
-# y = "computer"
-# print(x,y)
+x = 5
+print(x)
+y = x 
+x = "Geeks"
+print(x,y)
+y = "computer"
+print(x,y)
 
-# # Swapping of 2 variables
-# a = 5
-# b = 7
-# a, b = b, a
-# print(a, b) # Output: 7 5
+# Swapping of 2 variables
+a = 5
+b = 7
+a, b = b, a
+print(a, b) # Output: 7 5
 
-# # To count characters in a string
-# a = "Akhila"
-# c = 0 
-# for i in a:
-#     c += 1
-# print(c) # Output: 6 
-# #OR
-# print(len(a)) # Output: 6
+# To count characters in a string
+a = "Akhila"
+c = 0 
+for i in a:
+    c += 1
+print(c) # Output: 6 
+#OR
+print(len(a)) # Output: 6
 
-# import keyword
-# print("The list of keywords is : ")
-# print(keyword.kwlist)
+import keyword
+print("The list of keywords is : ")
+print(keyword.kwlist)
