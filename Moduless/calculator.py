26e2c20a5c21dd348e180f5b9d61c96ccc06b114
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-12-08 11:54:20 +0530

    first commit

--- Moduless/calculator.py
+++ Moduless/calculator.py
@@ -1,14 +1,18 @@
-# def add(a,b):
-#     return a + b
-# def subtract(a,b):
-#     return a - b 
-# def multiply(a,b):
-#     return a * b
-# def divide(a,b):
-#     if b == 0:
-#         return "Cannot divide by zero"
-#     return a / b
-# def modulus(a,b):
-#     return a % b    
-# def exponent(a,b):
-#     return a ** b   
+def add(a,b):
+    return a + b
+def subtract(a,b):
+    return a - b 
+def multiply(a,b):
+    return a * b
+def divide(a,b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+def modulus(a,b):
+    return a % b    
+def exponent(a,b):
+    return a ** b   
+def floor_divide(a,b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a // b
