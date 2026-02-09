cecf449e509ad8e53c81335917acc2fc578429b9
Kalagarla Akhila <akhilakalagarla@gmail.com>
2026-02-09 21:27:25 +0530

    initial commit

---
+++ Tests/control_flow.py
@@ -0,0 +1,14 @@
+tup = ("Akhila","Nandu","Akhila")
+for x in tup:
+    print(x)
+
+c = 0 
+while (c <= 3):
+    c = c + 1
+    print("Akhila")
+    
+for i in range(1, 5):
+    for j in range(i):
+        print(i, end = "")
+    print()
+
